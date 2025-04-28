24125381fa83d5203a173a95fb6419f0463dd50b
Aalee5699 <aalee5699@gmail.com>
2025-04-28 09:01:09 +0800

    Update ads.js

--- www/ads.js
+++ www/ads.js
@@ -6,9 +6,9 @@ var plugin_documentation = "https://cozycode.ca/post?pon=cordova-plugin-ads";
 //ads
 var test_ads = {
     'banner': "ca-app-pub-3940256099942544/6300978111",
-    'interstitial': "ca-app-pub-3940256099942544/1033173712",
-    'rewarded': "ca-app-pub-3940256099942544/5224354917",
-    'rewardedInterstitial': "ca-app-pub-3940256099942544/6978759866"
+    'interstitial': "ca-app-pub-7640572110579825/6826205715",
+    'rewarded': "ca-app-pub-7640572110579825/5951273598",
+    'rewardedInterstitial': "ca-app-pub-7640572110579825/6493364923"
 }
 var plugin_developer_ads = {
     'android' : {
